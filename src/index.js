0ecd7c1cf881d0add0daacf8f3eb84789a3b2f68
jaeyoung.choi <lactrious@gmail.com>
2018-07-17 13:01:38 +0900

    Add missing mappings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const getLogger = require('./logger');
 const config = require('./config');
 const aws = require('./aws');
-const { middleware, initialize } = require('./middleware');
+const { setup, middleware, initialize } = require('./middleware');
 
 module.exports = {
   getLogger,
   aws,
+  setup,
   middleware,
   initialize,
   config,
